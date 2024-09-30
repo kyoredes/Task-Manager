5c9c6d53e0a61bc8db7d8e66c58d10ad048b48b0
ddertaliss <ddertaliss@gmail.com>
2024-09-30 12:07:56 +0300

    tasks filter add

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,7 +1,25 @@
 import django_filters as filter
 from tasks.models import Task
+from django.utils.translation import gettext as translate
+from django import forms
+
 
 class TaskFilter(filter.FilterSet):
+    user_filter = filter.BooleanFilter(
+        label=translate('show only my tasks'),
+        widget=forms.CheckboxInput(),
+        method='filter_by_user',
+    )
+
     class Meta:
         model = Task
         fields = ['status', 'executor', 'label']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.get('request', None)
+        super().__init__(*args, **kwargs)
+
+    def filter_by_user(self, queryset, name, value):
+        if value:
+            return queryset.filter(author=self.request.user)
+        return queryset
---
+++ task_manager/tasks/tests/test_tasks_filter.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.urls import reverse
+from tasks.models import Task
+from statuses.models import Status
+from labels.models import Label
+from django.contrib.auth import get_user_model
+
+
+class TestTaskCase(TestCase):
+    def setUp(self):
+        users_data = {
+            'username': 'kaito',
+            'first_name': 'kaito',
+            'last_name': 'kaito',
+            'password1': '6754556876a',
+            'password2': '6754556876a',
+        }
+        response =  self.client.post(
+            reverse('create_user'), users_data
+        )
+        self.client.post(
+            reverse('login'), {'username': 'kaito', 'password': '6754556876a'}
+        )
+
+    def test_filter(self):
+        status = Status.objects.create(title='title')
+        label = Label.objects.create(name='name')
+        user = get_user_model().objects.all().get(username='kaito')
+        self.client.post(reverse('create_task'), {'title': 'title', 'description': 'sjhfkshk', 'status': status.id, 'executor': user.id})
+        # status filter
+        response = self.client.get(f'/tasks/?status={status.id}&executor=&label=')
+        self.assertEqual(response.status_code, 200)
+        # executor filter
+        response = self.client.get(f'/tasks/?status=&executor={user.id}&label=')
+        self.assertEqual(response.status_code, 200)
+        # label filter
+        response = self.client.get(f'/tasks/?status=&executor=&label={label.id}')
+        self.assertEqual(response.status_code, 200)
+        
